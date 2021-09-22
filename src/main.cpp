6b8b6c2e1a48188d94e8ad831d680b5f9401af6f
gellidao <gellidao@gmail.com>
2021-09-22 13:11:23 -0500

    Now the quaternion are readed from IMU BNO055

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <BasicLinearAlgebra.h>
 using namespace BLA;
+#include <BNOfunctions.h>
 #include <RadioPayload.h>
 
 Matrix<4, 1> qDeseado = {1.0, 1.0, 1.0, 1.0}; 
@@ -11,11 +12,15 @@ void setup() {
   Serial.begin(115200);
   
   InitialiceRadio() ;
+
+  delay(1000) ; 
+
+  InitialiceMPU(bno) ; 
 } // setup
 
 void loop() {
 
-  sended = send(qCurrent) ; 
+  sended = send(bno, qCurrent) ; 
   
   recieved = recieve(qDeseado);  
 
